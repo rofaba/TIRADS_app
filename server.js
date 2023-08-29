543736d14c6f49e34384adedd47fdc545e05730e
rofaba <rodrigofaure@gmail.com>
2023-08-29 15:30:20 -0400

    img display fixed

--- server.js
+++ server.js
@@ -2,22 +2,23 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'src')));
 
-    res.sendFile(path.resolve(__dirname + '/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/about.html'));
+    res.sendFile(path.resolve(__dirname, 'about.html'));
 });
 
 app.get('/bibliography', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/bibliography.html'));
+    res.sendFile(path.resolve(__dirname, 'bibliography.html'));
 });
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
-    
 });
 
 
